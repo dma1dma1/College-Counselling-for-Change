f0028fa6c190d207f0a5980424211c31cf822600
Daniel Ma <ziyuema@gmail.com>
2020-07-07 15:25:16 -0400

    FAQ done

--- FAQ/faqdata.js
+++ FAQ/faqdata.js
@@ -4,6 +4,10 @@ export default
         {
             "question": "Can I request a counselor?",
             "answer": "Yes! But we prefer you don't as we will try and match you based on your answers when you sign up."
+        },
+        {
+            "question": "This is a question",
+            "answer": "This is an answer"
         }
     ]
 }
